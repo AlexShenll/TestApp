47ebb3cc8f6d5cdf4021aa144658d5d2ba3fbb20
Alexander <jebarajalex@shenll.com>
2018-03-24 16:27:25 +0530

    Added code for task1

--- app/src/main/java/com/shenll/testapp/MainActivity.java
+++ app/src/main/java/com/shenll/testapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.shenll.testapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView env = findViewById(R.id.env);
+        env.setText(BuildConfig.environment);
     }
 }
